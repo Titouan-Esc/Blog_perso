e33e0fb6510f1f3375f7980091252ffc050fd9fb
Mes Morts <titouanescorneboueu@gmail.com>
2021-03-02 09:47:00 +0100

    Creation du form et mis en form (sans mauvais jeux de mot)

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className="formulaire">
+            <form>
+
+                <label htmlFor="titre">Ecrivez votre titre</label>
+                <input type="text" name="titre"/>
+
+                <label htmlFor="contenu">Votre contenu</label>
+                <textarea name="contenu" id="" cols="30" rows="10"></textarea>
+
+                <input type="submit" value="Publication"/>
+
+            </form>
         </div>
     )
 }
